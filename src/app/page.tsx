75b6b1c6359c97c2bea625fd52fcb153734f9a35
Agnaldo Burgo Junior <agnaldoburgojr@gmail.com>
2023-12-13 14:34:15 -0300

    feat: add tabs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import SettingsTabs from '@/components/settings-tabs'
+
 export default function Home() {
-  return <h1>hello</h1>
+  return (
+    <>
+      <h1 className="text-3xl font-medium text-zinc-900">Settings</h1>
+      <SettingsTabs />
+    </>
+  )
 }
